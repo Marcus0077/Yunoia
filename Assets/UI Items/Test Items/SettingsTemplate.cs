b37c3473c760b778c904251c183072a7070420fd
Melvin Cotto <mcottocotto@knights.ucf.edu>
2022-10-19 16:14:58 -0400

    Big ol Push for UI and My Whitebox

---
+++ Assets/UI Items/Test Items/SettingsTemplate.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+
+public class SettingsTemplate : EditorWindow
+{
+    [MenuItem("Window/UI Toolkit/SettingsTemplate")]
+    public static void ShowExample()
+    {
+        SettingsTemplate wnd = GetWindow<SettingsTemplate>();
+        wnd.titleContent = new GUIContent("SettingsTemplate");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        VisualElement label = new Label("Hello World! From C#");
+        root.Add(label);
+
+        // Import UXML
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UI Items/Test Items/SettingsTemplate.uxml");
+        VisualElement labelFromUXML = visualTree.Instantiate();
+        root.Add(labelFromUXML);
+
+        // A stylesheet can be added to a VisualElement.
+        // The style will be applied to the VisualElement and all of its children.
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/UI Items/Test Items/SettingsTemplate.uss");
+        VisualElement labelWithStyle = new Label("Hello World! With Style");
+        labelWithStyle.styleSheets.Add(styleSheet);
+        root.Add(labelWithStyle);
+    }
+}

26e5825f2e1d825c1cf3059b7859c740e8ee5ec0
Willyplayin <89488645+Willyplayin@users.noreply.github.com>
2022-10-22 15:09:43 -0400

    dub

--- Assets/LevelSwitchManager.cs
+++ Assets/LevelSwitchManager.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class LevelSwitchManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Rigidbody Player;
+
+    public List<Transform> LevelStarts;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("MutedGrappleEnd"))
+        {
+            Player.transform.position = LevelStarts[0].position;
+        }
+        else if (other.CompareTag("MutedPushEnd"))
+        {
+            Player.transform.position = LevelStarts[1].position;
+        }
+        if (other.CompareTag("MutedCloneEnd"))
+        {
+            Player.transform.position = LevelStarts[2].position;
+        }
     }
 }

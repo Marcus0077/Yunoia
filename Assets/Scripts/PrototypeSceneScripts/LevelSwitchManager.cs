2b0252b70a86e28297f4ab4f84de489d8b83a566
Willyplayin <89488645+Willyplayin@users.noreply.github.com>
2022-10-22 15:22:13 -0400

    update

---
+++ Assets/Scripts/PrototypeSceneScripts/LevelSwitchManager.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class LevelSwitchManager : MonoBehaviour
+{
+
+    public Rigidbody Player;
+
+    public List<Transform> LevelStarts;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MutedGrappleEnd"))
+        {
+            Player.transform.position = LevelStarts[0].position;
+        }
+        else if (other.CompareTag("MutedPushEnd"))
+        {
+            Player.transform.position = LevelStarts[1].position;
+        }
+        if (other.CompareTag("MutedCloneEnd"))
+        {
+            Player.transform.position = LevelStarts[2].position;
+        }
+    }
+}

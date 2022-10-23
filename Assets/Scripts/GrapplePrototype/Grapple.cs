931f031a94771daada4ba0a7df877d237f399d6d
murmur-myst <jon3murray@gmail.com>
2022-10-23 19:57:42 -0400

    FixedUpdate
    Grappling Hook swapped from Update to FixedUpdate (Edited Project Settings for Time to 0.0075 to edit fixed update parameters for complies per second). Should make this so that the functions occur universally across PCs. Framerate of User End was likely causing disparate issues across build playtests.

--- Assets/Scripts/GrapplePrototype/Grapple.cs
+++ Assets/Scripts/GrapplePrototype/Grapple.cs
@@ -31,7 +31,7 @@ public class Grapple : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (hook == null && shootHook.IsPressed())
         {

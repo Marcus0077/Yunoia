d17c20bb94928e8eea3b60f3f4f36dd95735fbf1
poiuyt0418 <poiuyt0418@hotmail.com>
2022-10-29 22:48:46 -0400

    removed unneeded comments

--- Assets/Scripts/PushPrototype/PushablePlatform.cs
+++ Assets/Scripts/PushPrototype/PushablePlatform.cs
@@ -4,21 +4,7 @@ using UnityEngine;
 
 public class PushablePlatform : Pushable
 {
-    //Vector3 playerPosition;
     Vector3 prevPosition;
-
-    //void OnTriggerEnter(Collider other)
-    //{
-    //    GameObject hit = other.gameObject;
-    //    if (hit.tag == "Player" || hit.tag == "Clone")
-    //    {
-    //        Vector3 tempOriginalScale = transform.localScale;
-    //        transform.localScale = Vector3.one;
-    //        playerPosition = transform.InverseTransformPoint(hit.transform.position);
-    //        transform.localScale = tempOriginalScale;
-    //        hit.transform.SetParent(transform);
-    //    }
-    //}
 
     void OnTriggerEnter(Collider other)
     {
@@ -29,15 +15,6 @@ public class PushablePlatform : Pushable
         }
     }
 
-    //void OnTriggerExit(Collider other)
-    //{
-    //    GameObject hit = other.gameObject;
-    //    if (hit.tag == "Player" || hit.tag == "Clone")
-    //    {
-    //        hit.transform.SetParent(null);
-    //    }
-    //}
-
     void OnTriggerStay(Collider other)
     {
         GameObject hit = other.gameObject;
@@ -47,38 +24,4 @@ public class PushablePlatform : Pushable
             prevPosition = transform.position;
         }
     }
-
-    //void FixedUpdate()
-    //{
-        //if(transform.childCount > 0)
-        //{
-        //    if (transform.GetChild(0).tag == "Player")
-        //    {
-        //        BasicMovementPlayer player = transform.GetChild(0).GetComponent<BasicMovementPlayer>();
-        //        if (player.isGrounded && player.moveDirection == Vector2.zero)
-        //        {
-        //            //transform.GetChild(0).transform.position = new Vector3(transform.position.x, transform.GetChild(0).transform.position.y, transform.position.z);
-        //            //transform.GetChild(0).transform.position = transform.position + playerPosition;
-        //        }
-        //        else
-        //        {
-        //            //playerPosition = transform.InverseTransformPoint(player.transform.position);
-        //        }
-        //    }
-        //    else if (transform.GetChild(0).tag == "Clone")
-        //    {
-        //        BasicMovementClone player = transform.GetChild(0).GetComponent<BasicMovementClone>();
-        //        if (player.isGrounded && player.moveDirection == Vector2.zero)
-        //        {
-        //            //transform.GetChild(0).transform.position = new Vector3(transform.position.x, transform.GetChild(0).transform.position.y, transform.position.z);
-        //            //transform.GetChild(0).transform.position = transform.position + playerPosition;
-        //        }
-        //        else
-        //        {
-        //            //playerPosition = transform.InverseTransformPoint(player.transform.position);
-        //        }
-        //    }
-        //}
-        //CapSpeed();
-    //}
 }

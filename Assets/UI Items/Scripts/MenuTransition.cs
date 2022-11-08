d0b694ffc579ccb9bea91a342befbd3564c636d0
Melon-Cotto <89477863+Melon-Cotto@users.noreply.github.com>
2022-11-08 18:00:18 -0500

    smaller Ui push

--- Assets/UI Items/Scripts/MenuTransition.cs
+++ Assets/UI Items/Scripts/MenuTransition.cs
@@ -8,9 +8,11 @@ public class MenuTransition : MonoBehaviour
 {
     public float xSize;
     public float ySize;
+    public CanvasGroup bg;
 
     public void Awake()
     {
+        bg.alpha = 0;
         transform.localScale = Vector2.zero;
     }
 
@@ -18,10 +20,12 @@ public class MenuTransition : MonoBehaviour
     {
         transform.LeanScale(new Vector2(xSize, ySize), 0.25f);
         gameObject.SetActive(true);
+        bg.LeanAlpha(1, 1f).setEaseOutExpo();
     }
 
     public void Close()
     {
         transform.LeanScale(Vector2.zero, 0.8f).setEaseInBack();
+        bg.LeanAlpha(0, 0.5f);
     }
 }
---
+++ Assets/UI Items/Scripts/OnButtonHover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnButtonHover : MonoBehaviour
+{
+    [SerializeField] private GameObject button;
+    public float hoverXSize, hoverYSize, exitXSize, exitYSize;
+    public Color hoverColor, nuetralColor;
+
+    public void OnHoverEnter()
+    {
+        LeanTween.color(button.GetComponent<RectTransform>(), hoverColor, .5f).setEaseInOutQuint();
+        transform.LeanScale(new Vector2(hoverXSize, hoverYSize), 0.125f).setEaseInOutQuint();
+    }
+
+    public void OnHoverExit()
+    {
+        LeanTween.color(button.GetComponent<RectTransform>(), nuetralColor, .5f).setEaseInOutQuint();
+        transform.LeanScale(new Vector2(exitXSize, exitYSize), 0.125f).setEaseInOutQuint();
+    }
+}

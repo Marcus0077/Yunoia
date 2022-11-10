bd072c0cd8670eaac6f715a3e04c1d171791537d
Melon-Cotto <89477863+Melon-Cotto@users.noreply.github.com>
2022-11-10 17:22:20 -0500

    Added UI to Denial Scenes

---
+++ Assets/UI/Scripts/AudioSettingTest.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class AudioSettingTest : MonoBehaviour
+{
+    private static readonly string BGMPref = "BGM Pref";
+    private static readonly string SFXPref = "SFX Pref";
+    private static readonly string MasPref = "Mas Pref";
+    private float bgmFloat, sfxFloat, masFloat;
+    public AudioSource bgmAudio;
+    public AudioSource[] sfxAudio;
+
+    void Awake()
+    {
+       ContinueSettings(); 
+    }
+
+    private void ContinueSettings()
+    {
+        bgmFloat = PlayerPrefs.GetFloat(BGMPref);
+        sfxFloat = PlayerPrefs.GetFloat(SFXPref);
+        masFloat = PlayerPrefs.GetFloat(MasPref);
+
+        bgmAudio.volume = bgmFloat;
+
+        AudioListener.volume = masFloat;
+
+        for(int i = 0; i< sfxAudio.Length; i++)
+        {
+            sfxAudio[i].volume = sfxFloat;
+        }
+    }
+}

cb1625277fdf4c7edf8e5083b2bbeb7caa812e53
warlordchieftain <89570269+warlordchieftain@users.noreply.github.com>
2022-11-16 22:36:27 -0500

    soundstruggle

---
+++ Assets/PlayerAnimationSounds.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationSounds : MonoBehaviour
+{
+    AudioSource animationSoundPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animationSoundPlayer = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+private void PlayerFootstepSound() 
+{
+    animationSoundPlayer.Play();
+}
+
+}
---
+++ Assets/Scripts/SoundRandomizer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundRandomizer : MonoBehaviour
+{
+    public AudioClip[] sounds;
+    private AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            source.clip = sounds[Random.Range(0, sounds.Length)];
+            source.Play();
+        }
+    }
+}

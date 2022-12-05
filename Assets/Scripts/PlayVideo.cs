b3f6d7ac563073387e6c38f7477c8e5a37a4a37b
warlordchieftain <89570269+warlordchieftain@users.noreply.github.com>
2022-12-04 20:25:08 -0500

    slight video player setup

---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideo : MonoBehaviour
+{
+
+    public GameObject videoPlayer;
+    public int timeToStop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.SetActive(false);
+    }
+
+    // Update is called once per frame
+  void OnTriggerEnter (Collider player){
+    
+    if (player.gameObject.tag == "Player")
+    {
+        videoPlayer.SetActive(true);
+        Destroy(videoPlayer, timeToStop);
+    }
+  }
+}

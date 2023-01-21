f7bc6bc17d2011299a7614b9b51a6218f6248e9f
Willyplayin <89488645+Willyplayin@users.noreply.github.com>
2023-01-21 14:23:15 -0500

    More Camera Prototyping

--- Assets/Scripts/MovementScripts/LimitedMovementCam.cs
+++ Assets/Scripts/MovementScripts/LimitedMovementCam.cs
@@ -11,18 +11,23 @@ public class LimitedMovementCam : MonoBehaviour
     public InputAction camMove;
     public InputAction playerMove;
     
-    // Camera Follow Sphere RigidBody
+    // Camera Follow Sphere(s) RigidBody
     public Rigidbody camFollowSphere;
+    public Rigidbody seekerSphere;
 
     // Movement Variables
     public Vector2 moveDirection;
-    public float accelerationValue;
+    public float baseAccelerationValue;
+    public float accelerationValueX;
+    public float accelerationValueY;
     public float moveSpeed;
     public float returnSpeed;
 
     public Vector3 playerPos;
 
     public float returnToPlayerTimer;
+
+    public CinemachineVirtualCamera curCamera;
     
     // Start is called before the first frame update
     void Awake()
@@ -31,10 +36,14 @@ public class LimitedMovementCam : MonoBehaviour
         
         playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
         transform.position = Vector3.MoveTowards(transform.position, playerPos, returnSpeed * Time.deltaTime);
+        
+        seekerSphere.position = camFollowSphere.position;
 
         returnSpeed = 20f;
         returnToPlayerTimer = 0f;
-        accelerationValue = 1f;
+        baseAccelerationValue = 1f;
+        accelerationValueX = 1f;
+        accelerationValueY = 1f;
         moveSpeed = 5f;
     }
 
@@ -45,22 +54,43 @@ public class LimitedMovementCam : MonoBehaviour
         
         if (camMove.IsPressed() && !playerMove.IsPressed())
         {
+            Debug.Log(camMove.ReadValue<Vector2>().x);
+            //Debug.Log(camMove.ReadValue<Vector2>().y);
+            
+            
             returnToPlayerTimer = 1.5f;
 
             moveDirection = (Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y - 90, -Vector3.forward) *
                                  camMove.ReadValue<Vector2>().normalized * moveSpeed);
 
-            camFollowSphere.velocity = new Vector3(moveDirection.y * accelerationValue,
-                    -moveDirection.x * accelerationValue, camFollowSphere.velocity.z);
+            camFollowSphere.velocity = new Vector3(moveDirection.y * accelerationValueY,
+                     -moveDirection.x * accelerationValueX, camFollowSphere.velocity.z);
+            
+            seekerSphere.velocity = new Vector3(moveDirection.y * accelerationValueY,
+                -moveDirection.x * accelerationValueX, camFollowSphere.velocity.z);
+            
+            // IF sphere moves to where camera can no longer go, stop sphere on that axis.
+            
+            // if ()
+            // {
+            //     accelerationValueY = 0f;
+            // }
+            // else if ()
+            // {
+            //     accelerationValueY = 1f;
+            // }
         }
         else if (!camMove.IsPressed() && !playerMove.IsPressed() && returnToPlayerTimer > 0)
         {
             camFollowSphere.velocity = Vector2.zero;
+            seekerSphere.velocity = Vector3.zero;
+            
             returnToPlayerTimer -= Time.deltaTime;
         }
         else
         {
             transform.position = Vector3.MoveTowards(transform.position, playerPos, returnSpeed * Time.deltaTime);
+            seekerSphere.transform.position = Vector3.MoveTowards(seekerSphere.transform.position, playerPos, returnSpeed * Time.deltaTime);
         }
     }
     

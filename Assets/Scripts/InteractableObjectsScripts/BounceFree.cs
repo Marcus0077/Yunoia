43afccfcbe3cfc7cf5b42addc323cb2991fc6d47
poiuyt0418 <poiuyt0418@hotmail.com>
2023-02-03 15:24:53 -0500

    Added more bounce

---
+++ Assets/Scripts/InteractableObjectsScripts/BounceFree.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceFree : MonoBehaviour
+{
+    [SerializeField]
+    float additiveForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        BasicMovement player = col.gameObject.GetComponent<BasicMovement>();
+        Debug.Log(player);
+        if (player != null)
+        {
+            player.playerRB.AddForce(Vector3.up * additiveForce);
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

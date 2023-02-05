7edcfed9d6a3c5973c1916b0000aca92835863c3
poiuyt0418 <poiuyt0418@hotmail.com>
2023-02-04 23:07:03 -0500

    navmesh fixes
    no bugs i hope

--- Assets/Scripts/Ai/FacelessPeople/NavMeshHandler.cs
+++ Assets/Scripts/Ai/FacelessPeople/NavMeshHandler.cs
@@ -6,19 +6,37 @@ using UnityEngine.AI;
 public class NavMeshHandler : MonoBehaviour
 {
     public NavMeshSurface[] surfaces;
-    
+    public NavMeshSurface[] scales;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public void BuildScaleNavMesh()
+    {
+        for (int i = 0; i < surfaces.Length; i++)
+        {
+            scales[i].BuildNavMesh();
+        }
+    }
+
+    public void BuildAllNavMesh()
+    {
+        for (int i = 0; i < surfaces.Length; i++)
+        {
+            surfaces[i].BuildNavMesh();
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < surfaces.Length; i++) 
-        {
-            surfaces [i].BuildNavMesh ();    
-        }    
+        //for (int i = 0; i < surfaces.Length; i++) 
+        //{
+        //    surfaces [i].BuildNavMesh ();
+        //    Debug.Log("a");
+        //}    
     }
 }
--- Assets/Scripts/InteractableObjectsScripts/ScaleControl.cs
+++ Assets/Scripts/InteractableObjectsScripts/ScaleControl.cs
@@ -10,17 +10,20 @@ public class ScaleControl : MonoBehaviour
     Vector3 oldLeftPos, oldRightPos;
     [SerializeField]
     float unitDistance = 1, speed = 1;
+    NavMeshHandler navHandler;
     [SerializeField]
     Vector3 axis;
     float compareValue = 0, frame;
     // Start is called before the first frame update
     void Start()
     {
+        frame = -1;
         leftPos = left.transform.position;
         rightPos = right.transform.position;
         oldLeftPos = leftPos;
         oldRightPos = rightPos;
         compareValue = 0;
+        navHandler = GameObject.FindGameObjectWithTag("NavmeshHandler").GetComponent<NavMeshHandler>();
     }
 
     // Update is called once per frame
@@ -33,11 +36,17 @@ public class ScaleControl : MonoBehaviour
             oldLeftPos = left.transform.position;
             oldRightPos = right.transform.position;
         }
-        if(frame * speed <= 60)
+        if(frame * speed <= 60 && frame >= 0)
         {
             left.transform.position = Vector3.Lerp(oldLeftPos, new Vector3(leftPos.x + unitDistance * axis.x * compareValue, leftPos.y + unitDistance * axis.y * compareValue, leftPos.z + unitDistance * axis.z * compareValue), frame / 60f * speed);
             right.transform.position = Vector3.Lerp(oldRightPos, new Vector3(rightPos.x + unitDistance * axis.x * -compareValue, rightPos.y + unitDistance * axis.y * -compareValue, rightPos.z + unitDistance * axis.z * -compareValue), frame / 60f * speed);
+            navHandler.BuildScaleNavMesh();
             frame++;
+        }
+        else if (frame != -1)
+        {
+            //navHandler.BuildAllNavMesh();
+            frame = -1;
         }
     }
 }

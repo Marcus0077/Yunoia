318edc9b6db373ae763c58b7a41426e407dcc979
Melon-Cotto <89477863+Melon-Cotto@users.noreply.github.com>
2023-02-09 17:06:16 -0500

    fixed up audio controls

---
+++ Assets/UI/Scripts/Opener.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opener : MonoBehaviour
+{
+    public GameObject Settings;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Settings.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

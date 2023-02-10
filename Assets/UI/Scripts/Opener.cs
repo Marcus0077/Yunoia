ef8b2ac20e4d161ffcab0419f3d3a41c900eccd0
Melon-Cotto <89477863+Melon-Cotto@users.noreply.github.com>
2023-02-10 15:16:00 -0500

    Added audio changes from sprint 5 branch

---
+++ Assets/UI/Scripts/Opener.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opener : MonoBehaviour
+{
+    public GameObject settings;
+    // Start is called before the first frame update
+    void Start()
+    {
+        settings.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

a2ea9941275c070e47ea5ada256c97ec17911e7d
Marcus0077 <marcus.werner@knights.ucf.edu>
2023-03-27 20:27:15 -0400

    updates

--- Assets/Scripts/SceneChanges/SpawnAcceptance.cs
+++ Assets/Scripts/SceneChanges/SpawnAcceptance.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
 
 public class SpawnAcceptance : MonoBehaviour
 {
+    public GameObject acceptTeleport;
+
     // Start is called before the first frame update
     void Awake()
     {
-        
+        if (DataManager.gameData.levelCompletion[(int)Levels.DEN]
+            && DataManager.gameData.levelCompletion[(int)Levels.ANG]
+            && DataManager.gameData.levelCompletion[(int)Levels.BAR]
+            && DataManager.gameData.levelCompletion[(int)Levels.DEP])
+        {
+            acceptTeleport.SetActive(true);
+        }
+        else
+        {
+            acceptTeleport.SetActive(false);
+        }
     }
 
 }

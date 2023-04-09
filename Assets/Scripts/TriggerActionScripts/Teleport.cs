f904f8e1669a6481617b9073db1b4c0adf543725
Willyplayin <89488645+Willyplayin@users.noreply.github.com>
2023-04-08 21:22:14 -0400

    Temp Push

--- Assets/Scripts/TriggerActionScripts/Teleport.cs
+++ Assets/Scripts/TriggerActionScripts/Teleport.cs
@@ -8,13 +8,22 @@ public class Teleport : MonoBehaviour
     public Transform teleportTarget;
     public GameObject player, terrain, ravine;
 
+    private GameManager gameManager;
+
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        gameManager
     }
 
     void OnTriggerEnter (Collider collider)
     {
+        
+    }
+
+    private IEnumerator FadeInOut()
+    {
+        
         player.transform.position = teleportTarget.transform.position;
 
         if (ravine != null && terrain != null)
@@ -22,5 +31,7 @@ public class Teleport : MonoBehaviour
             ravine.SetActive(false);
             terrain.SetActive(true);
         }
+        
+        LeanTween.color(GetComponent<RectTransform>(), Color.clear, 3f);
     }
 }

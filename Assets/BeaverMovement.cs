0362939aceb44fc4316a9893b5af9615707d26f0
guttersnipewaddle23 <89611739+guttersnipewaddle23@users.noreply.github.com>
2023-04-16 18:28:58 -0400

    MovementScriptFixed
    added smooth rotation

--- Assets/BeaverMovement.cs
+++ Assets/BeaverMovement.cs
@@ -5,25 +5,44 @@ using UnityEngine;
 public class BeaverMovement : MonoBehaviour
 
 {
-    public GameObject [] waypoints;
-    int current = 0;
-    float WPradius = 1; 
-    public float speed;
-    
+    public Transform[] waypoints;   // Array of waypoints for the object to move through
+    public float speed = 5f;        // Speed at which the object moves
+    private int currentWaypoint = 0; // Index of the current waypoint
+    private Vector3 direction;      // Direction the object is moving
+    private Quaternion rotation;   // Rotation of the object
+
+    void Start()
+    {
+        // Set the initial direction to the first waypoint
+        direction = (waypoints[currentWaypoint].position - transform.position).normalized;
+
+        // Set the initial rotation to face the direction of movement
+        rotation = Quaternion.LookRotation(direction);
+        transform.rotation = rotation;
+    }
 
     void Update()
     {
-        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+        // Move the object towards the current waypoint
+        transform.position += direction * speed * Time.deltaTime;
+
+        // If the object is close enough to the current waypoint, move to the next one
+        if (Vector3.Distance(transform.position, waypoints[currentWaypoint].position) < 0.1f)
         {
-            current++;
-            if (current >= waypoints.Length)
+            currentWaypoint++;
+
+            // If the object has reached the final waypoint, reset to the first waypoint
+            if (currentWaypoint >= waypoints.Length)
             {
-                current = 0;
+                currentWaypoint = 0;
             }
+
+            // Set the new direction and rotation based on the new waypoint
+            direction = (waypoints[currentWaypoint].position - transform.position).normalized;
+            rotation = Quaternion.LookRotation(direction);
         }
-        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
 
-        
-    } 
-
+        // Rotate the object to face the direction of movement
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 5f);
+    }
 }

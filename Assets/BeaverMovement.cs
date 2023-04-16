45b33e528978af3d0c7f040a18bb6396671d5783
guttersnipewaddle23 <89611739+guttersnipewaddle23@users.noreply.github.com>
2023-04-16 17:45:10 -0400

    walking

--- Assets/BeaverMovement.cs
+++ Assets/BeaverMovement.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BeaverMovement : MonoBehaviour
-{
-        public float speed = 2;
-        void Update()
-        {
-            transform.position = new Vector3(Mathf.PingPong(Time.time * speed, 4), transform.position.y, transform.position.z);
-        }
 
-    }
+{
+    public GameObject[] waypoints;
+    int current = 0;
+    float rotSpeed;
+    float WPradius = 1; 
+    public float speed;
+
+    void Update()
+    {
+        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+        {
+            current++;
+            if (current >= waypoints.length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.postion, waypoints[current].transform.position, Time.deltaTime * speed); 
+
+    } 
+
+}
